58a1c078c99fc8ee94ffcfac0af26f055ad72ae5
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-11-27 20:38:43 +0100

    Operaciones
    Act

--- 3-operaciones/operaciones.js
+++ 3-operaciones/operaciones.js
@@ -1,5 +1,11 @@
 //Creo dos variables con numero 
-let.numero1 = 5;
-let.numero2 = 3;
+let numero1 = 5;
+let numero2 = 3;
 
+//Sumo los dos numeros y lo guardo en la variable suma
+let suma = numero1 + numero2;
 documento.write("Sumar los numero " + numero1 + " y " + numero2 + "es igual a " + suma);
+
+//Resto los dos numeros y lo guardo en la variable resta
+let resta = numero1 + numero2;
+document.write("Resta los numero " + numero1 + "y " + numero2 + "es igual a" + resta);
