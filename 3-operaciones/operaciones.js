b61147843217067d593d3a448600fbf546cc8e2a
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-11-27 20:52:55 +0100

    multi y div
    Act

--- 3-operaciones/operaciones.js
+++ 3-operaciones/operaciones.js
@@ -4,8 +4,20 @@ let numero2 = 3;
 
 //Sumo los dos numeros y lo guardo en la variable suma
 let suma = numero1 + numero2;
-documento.write("Sumar los numero " + numero1 + " y " + numero2 + "es igual a " + suma);
+
+documento.write("Sumar los numeros " + numero1 + " y " + numero2 + "es igual a " + suma);
 
 //Resto los dos numeros y lo guardo en la variable resta
-let resta = numero1 + numero2;
-document.write("Resta los numero " + numero1 + "y " + numero2 + "es igual a" + resta);
+let resta = numero1 - numero2;
+
+document.write("Resta los numeros " + numero1 + "y " + numero2 + "es igual a" + resta);
+
+//Divido los dos numeros y lo guardo en la variable division
+let division = numero1 / numero2;
+
+document.write("dividir los numeros " + numero1 + "y " + numero2 + "es igual a" + division);
+
+//Multiplico los dos numeros y lo guardo en la variable multiplicacion 
+let multiplicacion = numero1 * numero2;
+
+document.write("multiplico los numeros " + numero1 + "y " + numero2 + "es igual a" + multiplicacion);
