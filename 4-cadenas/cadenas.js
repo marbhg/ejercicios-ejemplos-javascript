e58d1c3a8e3a547c49f9bd702f583e971be8cd21
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-11-29 22:22:53 +0100

    act
    Variables

--- 4-cadenas/cadenas.js
+++ 4-cadenas/cadenas.js
@@ -13,7 +13,16 @@ document.write("SaludoCompleto " + SaludoCompleto + "</br>");
 document.write("Longitud de la cadena: " + SaludoCompleto.length + "<br>");
 
 //Mostrar la cadena en mayusculas
-document.write("Saludo en mayusculas: " + SaludoCompleto.toUpperCase())
+document.write("Saludo en mayusculas: " + SaludoCompleto.toUpperCase() + "<br>")
 
 //Mostrar la cadena en minuscula
-document.write("Saludo en minuscula: " + SaludoCompleto.toLowerCase())
+document.write("Saludo en minuscula: " + SaludoCompleto.toLowerCase() + "<br>")
+
+//Reemplazar texto de una cadena
+let despedida = SaludoCompleto.replace("Hola", "Adios");
+document.write("Saludo con palabra reemplazada: " + despedida);
+
+//Includes muestra true si la cadena contiene la palabra indicada o false sino. Distingue mayusculas y minusculas.
+document.write("Saludo Completo tiene la palabra Mundo? " + SaludoCompleto.includes("Mundo") + "<br>");
+document.write("Saludo Completo tiene la palabra MUNDO? " + SaludoCompleto.includes("MUNDO") + "<br>")
+document.write("Saludo Completo tiene la palabra marta? " + SaludoCompleto.includes("marta") + "<br>");
