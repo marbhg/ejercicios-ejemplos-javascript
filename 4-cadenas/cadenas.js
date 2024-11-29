0fb1a015b784629cd7674a488126feba951b79f7
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-11-29 20:43:53 +0100

    Variables
    Cadenas

--- 4-cadenas/cadenas.js
+++ 4-cadenas/cadenas.js
@@ -1,6 +1,19 @@
 
+//Declarar variables con candenas
 let saludo = "Hola Mundo";
 let saludo2 = "Hello Word";
 
+//Concatenar cadenas
 let SaludoCompleto = saludo + " " + saludo2;
-document.write("SaludoCompleto " + SaludoCompleto);
+
+//Mostrar cadenas
+document.write("SaludoCompleto " + SaludoCompleto + "</br>");
+
+//Mostrar la longitud de una cadena
+document.write("Longitud de la cadena: " + SaludoCompleto.length + "<br>");
+
+//Mostrar la cadena en mayusculas
+document.write("Saludo en mayusculas: " + SaludoCompleto.toUpperCase())
+
+//Mostrar la cadena en minuscula
+document.write("Saludo en minuscula: " + SaludoCompleto.toLowerCase())
