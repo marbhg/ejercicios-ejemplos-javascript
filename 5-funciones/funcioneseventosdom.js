36259519caba81922044f21e1abd3b156037a667
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-01 23:28:48 +0100

    act
    funciones

--- 5-funciones/funcioneseventosdom.js
+++ 5-funciones/funcioneseventosdom.js
@@ -1,11 +1,24 @@
 //Crear una funcion
 function saludar() {
   //Obtenemos el elemento que queremos modificar del HTML
-  document.getElementById("parrafo").textContent = "Hola Mundo";
+  document.getElementById("parrafo").textContent = "Hola Mundo"
 }
 
 function despedir() {
+  document.getElementById("parrafo").textContent = "Adios"
+}
 
-  document.getElementById("parrafo").textContent = "despedir"
+function cambiarColor() {
+  //Obtenemos el elemento del que queremos cambiar el color por el id
+  document.getElementById("parrafo").style.color = "red"
+}
 
+function ocultar() {
+  //Obtenemos el elemento con id parrafo y cambiamos display a none para ocultarlo
+  document.getElementById("parrafo").style.display = "none"
+}
+
+function mostrar() {
+  //Obtenemos el elemento displau parrafo y cambiamos display a block para mostrarlo
+  document.getElementById("parrafo").style.display = "block"
 }
