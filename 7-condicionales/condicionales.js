8f178629f917ca4ab365b6ed1e0e0cec8657580e
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-08 19:53:31 +0100

    condicionales
    ACT

--- 7-condicionales/condicionales.js
+++ 7-condicionales/condicionales.js
@@ -1,35 +1,47 @@
-function comprobarEdad(){
+function comprobarEdad() {
   //Guardamos la edad 
- let edad = document.getElementById("edad").valueAsNumber;
- let mensaje = "";
- 
- //Segun edad, damos un valor u otro al mensaje 
+  let edad = document.getElementById("edad").valueAsNumber;
+  let mensaje = "";
 
- if(edad > 18){
-  mensaje = "El usuario tiene mas de 18 años";
- }
- else if(edad === 18) {
-mensaje = "El usuario tiene 18 años";
- }
-else {
-  mensaje = "El usuario tiene menos de 18 años";
+  //Segun edad, damos un valor u otro al mensaje 
+
+  if (edad > 18) {
+    mensaje = "El usuario tiene mas de 18 años";
+  }
+  else if (edad === 18) {
+    mensaje = "El usuario tiene 18 años";
+  }
+  else {
+    mensaje = "El usuario tiene menos de 18 años";
+  }
+  //mostramos el mensaje 
+  document.getElementById("mensajeedad").textContent = mensaje;
+
 }
-//mostramos el mensaje 
-document.getElementById("mensajeedad").textContent = mensaje;
-
- }
-function adivinar(){
+function adivinar() {
   //Creamos una constante con el texto ganador
   let colorelegido = document.getElementById("colorpremio").value;
   ///Guardamos el valor selecionado por la variable del color elegido por el usuario
   let colorganador = "lila";
-  let mensaje ="";
-//Comprobamos si el color elegido es igual al ganador o no. Pasamos a mayusculas todos los textos para evitar errores en las comparaciones si tienen mayusculas o minuscukas diferentes, 
-  if("colorelegido.toUpperCase() === colorganador.toUpperCase()"){
+  let mensaje = "";
+  //Comprobamos si el color elegido es igual al ganador o no. Pasamos a mayusculas todos los textos para evitar errores en las comparaciones si tienen mayusculas o minuscukas diferentes, 
+  if ("colorelegido.toUpperCase() === colorganador.toUpperCase()") {
     mensaje = "Ha ganado";
-  } else { 
+  } else {
     mensaje = "Ha perdido";
- }
-document.getElementById("mensajepremio").textContent = mensaje; 
+  }
+  document.getElementById("mensajepremio").textContent = mensaje;
+}
+function comprobarEdadValidada() {
+  //Obtenemos el formulario 
+  let formulario = get.getElementById("formularioedad");
+
+  if (formulario.checkValidity()) {
+    comprobarEdad();
+  }
+  else {
+    document.getElementById("mensajeedad").tecContent = "Error edad invalida";
+
+  }
 }
 
